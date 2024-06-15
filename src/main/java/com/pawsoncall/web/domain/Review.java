30cfd436751182ca5af9c1cfd26ca2300c1eb46b
ChaoyangD <chaoyangdmaster@gmail.com>
2024-06-14 17:02:39 -0700

    Review table

---
+++ src/main/java/com/pawsoncall/web/domain/Review.java
@@ -0,0 +1,80 @@
+package com.pawsoncall.web.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String reviewText;
+    private int rating;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    Review() {}
+
+    Review(String reviewText, int rating) {
+        this.reviewText = reviewText;
+        this.rating = rating;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String toString() {
+        return "Review [id=" + id + ", reviewText=" + reviewText + ", rating=" + rating + "]";
+    }
+
+}
