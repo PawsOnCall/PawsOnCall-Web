07a0886c067306bf701fe637c269cab40540e69d
y.wang8 <y.wang8@student.fdu.edu>
2024-06-30 11:53:08 +0800

    Revert "init pet table"
    This reverts commit 6416dec0025d849f5ea3082f8ad03853f776ee22.

--- src/main/java/com/pawsoncall/web/mapper/PetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pawsoncall.web.mapper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.pawsoncall.web.domain.Pet;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {
-}
